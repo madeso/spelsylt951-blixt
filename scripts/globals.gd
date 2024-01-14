2899f72104beca95167dbc7b882825cbccf9151d
Gustav <sir.gustav.the.coder@gmail.com>
2024-01-14 21:47:22 +0100

    Last levels

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -4,7 +4,15 @@ var level = 0
 
 func next_level():
 	print("next level")
-	level = (level + 1) % 1
+	level = (level + 1) % 5
 	
 	if level == 0:
 		get_tree().change_scene_to_file("res://levels/level_1.tscn")
+	if level == 1:
+		get_tree().change_scene_to_file("res://levels/level_2.tscn")
+	if level == 2:
+		get_tree().change_scene_to_file("res://levels/level_3.tscn")
+	if level == 3:
+		get_tree().change_scene_to_file("res://levels/level_4.tscn")
+	if level == 4:
+		get_tree().change_scene_to_file("res://levels/level_5.tscn")
